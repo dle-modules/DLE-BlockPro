7e4f2d9ef101c38ffd82aab735ad688e34dd7f60
Pavel Belousov <pafnuty10@gmail.com>
2014-09-21 19:32:02 +0400

    Update blockpro.php

--- upload/engine/inc/blockpro.php
+++ upload/engine/inc/blockpro.php
@@ -21,7 +21,7 @@ if ($member_id['user_group'] != '1') {
 define('MODULE_DIR', ENGINE_DIR . '/modules/base/admin/blockpro/');
 
 $moduleName = 'blockpro';
-$moduleVersion = '4.2.2';
+$moduleVersion = '4.2.4';
 
 $moderate           = $_REQUEST['moderate'];
 $moderate_checked   = ($moderate) ? 'checked' : '' ;
