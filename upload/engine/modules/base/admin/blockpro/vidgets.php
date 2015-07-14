0cab060c0ca3c5643476b0547e5265ab1c854686
Pavel Belousov <pafnuty10@gmail.com>
2015-07-14 23:32:43 +0300

    Улучшение вкладки с виджетами
    - Добавлен вывод строки подключения виджета
    - Добавлена кнопка предпросмотра виджета,. полезно, когда нужно создать
    новый виджет на основе имеющегося или просто посмотреть результат
    генерации виджета.

--- upload/engine/modules/base/admin/blockpro/vidgets.php
+++ upload/engine/modules/base/admin/blockpro/vidgets.php
@@ -41,9 +41,61 @@ if (isset($_POST['blockId']) && $_POST['widgetDelete'] == 'Y') {
 		<h2>Виджеты для вывода на сторонних сайтах</h2>
 		<?if (count($arVidgets) > 0):?>
 			<?foreach ($arVidgets as $key => $vidgetItem):?>
+				<?
+					$params = unserialize($vidgetItem['params']);
+					$vidgetModuleUrl = 'include file="engine/modules/base/blockpro.php';
+					$vidgetParamsUrl = array();
+
+					// Удаляем значения по умолчанию за ненадобностью.
+					if ($params['template'] == 'blockpro/blockpro') {												
+						unset($params['template']);
+					}
+					if ($params['cachePrefix'] == 'news') {												
+						unset($params['cachePrefix']);
+					}
+					if ($params['limit'] == 10) {												
+						unset($params['limit']);
+					}
+					if ($params['fixed'] == 'yes') {												
+						unset($params['fixed']);
+					}
+					if ($params['allowMain'] == 'yes') {												
+						unset($params['allowMain']);
+					}
+					if ($params['xfSearchLogic'] == 'on') {												
+						unset($params['xfSearchLogic']);
+					}
+					if ($params['sort'] == 'top') {												
+						unset($params['sort']);
+					}
+					if ($params['order'] == 'new') {												
+						unset($params['order']);
+					}
+					if ($params['pageNum'] == 1) {												
+						unset($params['pageNum']);
+					}
+					if ($params['navStyle'] == 'classic') {												
+						unset($params['navStyle']);
+					}
+					if ($params['xfSortType'] == 'int') {												
+						unset($params['xfSortType']);
+					}
+
+					$filteredparams = array_filter($params);
+					if (count($filteredparams) > 0) {
+						$vidgetModuleUrl .='?';
+					}
+					foreach (array_filter($params) as $key => $value) {
+						$vidgetParamsUrl[] = $key . '=' . $value;
+					}
+					$vidgetModuleUrlParams = implode('&amp;', $vidgetParamsUrl);
+
+					$vidgetModuleUrl .= $vidgetModuleUrlParams;
+				?>
+		
 				<div class="widget-item content">
 					<div class="col col-mb-9">
-						<b class="text-muted">[<?=$vidgetItem['id']?>]</b> <h3 class="m0 mr10 fz20 d-ib"><a href="/blockpro.php?block=<?=$vidgetItem['block_id']?>" target="_blank"><?=$vidgetItem['name']?></a></h3> (создан <?=$vidgetItem['date']?>)
+						<b class="text-muted">[<?=$vidgetItem['id']?>]</b> <h3 class="m0 mr10 fz20 d-ib"><a href="/blockpro.php?block=<?=$vidgetItem['block_id']?>" target="_blank"><?=$vidgetItem['name']?></a></h3> (создан <?=$vidgetItem['date']?>) <a href="?mod=blockpro&setPreview=y&<?=$vidgetModuleUrlParams?>#tab2" class="btn btn-small" title="Предпросмотр виджета или создание нового на основе текущего">Предпросмотр</a>
 					</div>
 					<div class="col col-mb-3 ta-right">
 						<span class="btn btn-small delete-widget" data-widget-action="<?$PHP_SELF?>?mod=blockpro" data-widget-id="<?=$vidgetItem['id']?>" data-widget-name="<?=$vidgetItem['name']?>">удалить</span>
@@ -54,7 +106,11 @@ if (isset($_POST['blockId']) && $_POST['widgetDelete'] == 'Y') {
 <div id="<?=$vidgetItem['block_id']?>"></div></textarea>
 						
 						<h4>Код для вывода RSS:</h4>
-						<textarea rows="1" readonly class="input input-block-level code"><?=$config['http_home_url']?>blockpro.php?channel=<?=$vidgetItem['block_id']?></textarea>						
+						<textarea rows="1" readonly class="input input-block-level code"><?=$config['http_home_url']?>blockpro.php?channel=<?=$vidgetItem['block_id']?></textarea>
+
+						<h4>Строка подключения:</h4>
+						<textarea rows="1" class="input input-block-level code">{<?=$vidgetModuleUrl?>"}</textarea>	
+										
 					</div>
 				</div>
 			<?endforeach?>
